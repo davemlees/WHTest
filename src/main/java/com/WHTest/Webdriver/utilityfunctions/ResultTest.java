20b482c382407cf1d91812483d1a4c7f6942fb7f
davemlees <davemlees@yahoo.co.uk>
2016-11-18 15:34:26 +0000

    Update ResultTest.java

--- src/main/java/com/WHTest/Webdriver/utilityfunctions/ResultTest.java
+++ src/main/java/com/WHTest/Webdriver/utilityfunctions/ResultTest.java
@@ -12,15 +12,7 @@ public class ResultTest {
                     this.resultText = resultText;
                     this.intTestStep = intTestStep;
                 }
-            
-//                //Added by Mathi for linking the snapshots with the fail status
- //               public ResultTest(int intTestStep, String result, String resultText, String filePath) {
-  //                  this.result = result;
-  //                  this.resultText = resultText;
-   //                 this.intTestStep = intTestStep;
-    //                this.filePath = filePath;
-     //           }
-            
+                       
                 public void setResult(String result) {
                     this.result = result;
                 }
